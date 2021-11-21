59b84220a1884fc4bc0d984359aed84b17c1f101
RICHIwon <94795579+RICHIwon@users.noreply.github.com>
2021-11-21 18:49:00 +0200

    Add files via upload

---
+++ prototype-2/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.slider__header').slick({
+	arrows: false,
+	dots: true,
+	infinite: true,
+	speed: 300,
+	slidesToShow: 1,
+	slidesToScroll: 1,/*
+	autoplay: true,
+  	autoplaySpeed: 5000,*/
+	});
+});
